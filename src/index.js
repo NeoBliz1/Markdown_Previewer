c4e421112ad6eec78decbde450bf31d0ee382a96
neobliz1 <49137013+NeoBliz1@users.noreply.github.com>
2026-05-13 13:53:07 +0500

    feat: Refactor and simplify projectThis commit refactors the project by removing the build directory, updating the .gitignore file, and deleting unused configuration files like .babelrc and workspace.json. Dependencies in package.json and package-lock.json were also updated. Additionally, the App.js and index.js components have been updated.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import createRoot from 'react-dom';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+createRoot.render(<App />, rootElement);
